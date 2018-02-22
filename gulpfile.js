97bd9e630e044e3d20c695eb50fa5f58ba61364d
Alex Quot <alexquot@gmail.com>
2018-02-22 06:25:06 -0500

    Make .progress look more like the material one; style quotes; begin to rewrite docs

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const tildeImporter = require('node-sass-tilde-importer');
+const pug = require('gulp-pug');
 
-gulp.task('build', function () {
+gulp.task('scss', function () {
   return gulp.src('./scss/matilda.scss')
     .pipe(
       sass({
@@ -16,6 +17,21 @@ gulp.task('build', function () {
     .pipe(gulp.dest('./docs/'));
 });
 
+gulp.task('views', function () {
+  return gulp.src('views/*.pug')
+    .pipe(pug({}))
+    .on('error', function (e) {
+      console.log(e);
+    })
+    .pipe(gulp.dest('./docs/'));
+});
+
 gulp.task('watch', function () {
-  gulp.watch('./scss/**/*.scss', ['build']);
+  gulp.watch([
+    './scss/**/*.scss',
+    './views/**/*.pug'
+  ], [
+    'scss',
+    'views'
+  ]);
 });
