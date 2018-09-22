962d4c693016c499c9e66d80fbd419cf887609b8
albnnc <albnnc@gmail.com>
2018-09-22 15:56:54 -0400

    Fix base href

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withSass = require('@zeit/next-sass');
 
 const isProduction = process.env.NODE_ENV === 'production';
-const baseHref = isProduction ? '/matilda' : '';
+const baseHref = isProduction ? '/matilda/' : ''; // trailing slash is important!
 
 const config = {
   assetPrefix: baseHref,
